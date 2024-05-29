6fcf09e387b4c7a3b7fbdf357237ba2ddff9b27c
Michal Čaniga <michal.caniga@instea.sk>
2024-05-29 20:42:14 +0200

    Remove amqp starter

--- bmicroservice/src/main/java/com/instea/presentation/bmicroservice/controller/DataController.java
+++ bmicroservice/src/main/java/com/instea/presentation/bmicroservice/controller/DataController.java
@@ -1,12 +1,14 @@
 package com.instea.presentation.bmicroservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 
 /**
  * Controller to process data requests.
